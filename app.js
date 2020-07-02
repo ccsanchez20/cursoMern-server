4094696ad513b8e0dc92cf05e4663a7c072e99ee
ccsanchez20 <cristian.cs.software@gmail.com>
2020-07-02 10:47:50 +0200

    Formulario de registro terminado

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ app.use(bodyParser.json());
 
 // Configure Header HTTP
 // ...
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Router Basic
 // ...
