1426ede63fdc69b2b856fef3acb86edc954a3054
ccsanchez20 <cristian.cs.software@gmail.com>
2020-07-04 20:18:31 +0200

    Sincronizado app.js

--- app.js
+++ app.js
@@ -14,21 +14,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Configure Header HTTP
-<<<<<<< HEAD
-=======
-// ...
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-// Router Basic
-// ...
-app.use(`/api/${API_VERSION}`, userRoutes);
-
-// Configure Header HTTP
->>>>>>> 4094696ad513b8e0dc92cf05e4663a7c072e99ee
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
